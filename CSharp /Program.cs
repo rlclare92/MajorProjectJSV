50dd6dcab3fd3110075b9290375e8f2e5ed224dd
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-20 09:17:03 +0000

    commit 3

--- CSharp /Program.cs
+++ CSharp /Program.cs
@@ -6,171 +6,44 @@ namespace CSharp
     {
         string input;
         int playerAgeInput;
-        int playerInput;
-        int userBalance; 
-        int playersTotalOfFirstDraw;
-        int dealersTotalOfFirstDraw;
 
-    //     public void ageRestriction()
-    //     {
-    //     Console.WriteLine("Please enter age");
-    //     input = Console.ReadLine();
-    //     playerAgeInput = Convert.ToInt32(input);
-    //     // Console.WriteLine(playerAgeInput);
+        public void ageRestriction()
+        {
+            Console.WriteLine("Please enter age");
+            input = Console.ReadLine();
+            playerAgeInput = Convert.ToInt32(input);
+            // Console.WriteLine(playerAgeInput);
 
-    //      if (playerAgeInput >= 18) {
-    //     Console.WriteLine("Welcome, please select a game from the options, BlackJack or Roulette");
-    //     gameOptions();
-    // } else {
-    //     Console.WriteLine("Sorry you are under age, you have to be over 18 to play");
-    // }
-    // }
-
-    // public void gameOptions()
-    // {
-    //     input = Console.ReadLine();
-
-    //     if(input == "BlackJack"){
-    //         blackJackGame();
-    //     } else if (input == "Roulette"){
-    //         // rouletteTableGame();
-    // }
-    // }
-
-    public void blackJackGame()
-    {
-        Console.WriteLine("Please place your bets £");
-        input = Console.ReadLine();
-        playerInput = Convert.ToInt32(input);
-        Console.WriteLine("You have placed {0}, Dealer has placed {0}", playerInput);
-        playersHand();
-    }
-
-    public void playersHand()
-    {
-        Random rnd = new Random();
-        int playersFirstCard = rnd.Next(1,12);
-        Console.WriteLine("Your first card is: {0}", playersFirstCard);
-        int playersSecondCard = rnd.Next(1,12);
-        Console.WriteLine("Your second card is: {0}", playersSecondCard);
-        playersTotalOfFirstDraw = (playersFirstCard + playersSecondCard);
-        Console.WriteLine("Your total is: {0}", playersTotalOfFirstDraw);
-        if(playersTotalOfFirstDraw == 21)
-        {
-            Console.WriteLine("BlackJack!!, Player wins");
-            PlayerWinnings();
-        } else {
-            playerChoices(playersTotalOfFirstDraw);
+            if (playerAgeInput >= 18)
+            {
+                Console.WriteLine("Welcome, please select a game from the options, BlackJack or Roulette");
+                gameOptions();
+            }
+            else
+            {
+                Console.WriteLine("Sorry you are under age, you have to be over 18 to play");
+            }
         }
-    }
-
-        public void playerChoices(int playersTotalOfFirstDraw)
+        public void gameOptions()
         {
-        Console.WriteLine("Hit or Stand?");
-        input = Console.ReadLine();
-        Random rnd = new Random();
-        if(input == "Hit")
-        {
-            int playersNextCard = rnd.Next(1,12);
-            Console.WriteLine("Your next card is {0}", playersNextCard);
-            playersTotalOfFirstDraw += playersNextCard;
-            Console.WriteLine("Your current total is: {0}", playersTotalOfFirstDraw);
-            ScoreCheckPlayer(playersTotalOfFirstDraw);
+            input = Console.ReadLine();
+            if (input == "BlackJack")
+            {
+                classes.BlackJack blackJackGamePlay = new classes.BlackJack();
+            }
+            else if (input == "Roulette")
+            {
+                // rouletteTableGame();
+            }
         }
-        else if (input == "Stand")
-        {
-          Console.WriteLine("You have stand at {0}, dealers turn",playersTotalOfFirstDraw);
-          Console.Read();
-          dealersHand();
-        }
-    }
-    public void ScoreCheckPlayer(int playersTotalOfFirstDraw)
-    {
-        if(playersTotalOfFirstDraw >= 22)
-        {
-            Console.WriteLine("Bust");
-            dealersWinning();
-        } else if (playersTotalOfFirstDraw == 21)
-        {
-             Console.WriteLine("BlackJack!!");
-             PlayerWinnings();
-        }
-        else {
-            playerChoices(playersTotalOfFirstDraw);
-        }
-    }
-    public void dealersHand()
-    {
-        Random rnd = new Random();
-        int dealersFirstCard = rnd.Next(1,12);
-        Console.WriteLine("Dealer's first card is: {0}", dealersFirstCard);
-        int dealersSecondCard = rnd.Next(1,12);
-        Console.WriteLine("Dealer's second card is: {0}", dealersSecondCard);
-        int dealersTotalOfFirstDraw = (dealersFirstCard + dealersSecondCard);
-        Console.WriteLine("Dealer's total is: {0}", dealersTotalOfFirstDraw);
-        if(dealersTotalOfFirstDraw == 21)
-        {
-            Console.WriteLine("BlackJack!! Player Wins");
-             PlayerWinnings();
-        } else {
-            dealersChoice(dealersTotalOfFirstDraw);
-        }
-    }
-
-    public void dealersChoice(int dealersTotalOfFirstDraw)
-    {
-        Random rnd = new Random();
-        // Console.WriteLine("{0}", dealersTotalOfFirstDraw);
-        if(dealersTotalOfFirstDraw <= playersTotalOfFirstDraw)
-        {
-            int dealersNextCard = rnd.Next(1,12);
-            Console.WriteLine("Dealer's next card is {0}", dealersNextCard);
-            dealersTotalOfFirstDraw += dealersNextCard;
-            Console.WriteLine("Dealer's current total is: {0}", dealersTotalOfFirstDraw);
-            ScoreCheckDealer(dealersTotalOfFirstDraw);
-        }
-        else if (dealersTotalOfFirstDraw > playersTotalOfFirstDraw)
-        {
-          Console.WriteLine("Dealer Wins!!");
-          dealersWinning();
-        }
-    }
-    public void ScoreCheckDealer(int dealersTotalOfFirstDraw)
-    {
-        if(dealersTotalOfFirstDraw> 22)
-        {
-            Console.WriteLine("Dealer bust, player wins");
-            PlayerWinnings();
-
-        } else if (dealersTotalOfFirstDraw == 21)
-        {
-             Console.WriteLine("BlackJack!! Dealer wins!!");
-            dealersWinning();
-        }
-        else {
-            dealersChoice(dealersTotalOfFirstDraw);
-        }
-    }
-    public void PlayerWinnings()
-    {
-          playerInput += userBalance;
-          Console.WriteLine("Player wins {0}", playerInput);
-          Console.WriteLine("PlayerWinnings updated balance {0}", userBalance);
-    }
-
-    public void dealersWinning()
-    {
-        playerInput -= userBalance;
-        Console.WriteLine("Dealer wins {0}", playerInput);
-        Console.WriteLine("User balance is: {0}", userBalance);
-    }
 
         static void Main(string[] args)
         {
-            // Program casinoGame = new Program();
-            // casinoGame.ageRestriction();
             Program casinoGame = new Program();
-            casinoGame.blackJackGame();
+            casinoGame.ageRestriction();
+
+            classes.BlackJack blackJackGamePlay = new classes.BlackJack();
+            blackJackGamePlay.blackJackGame();
         }
     }
 }
---
+++ CSharp /blackJackGame.cs
@@ -0,0 +1,159 @@
+using System;
+
+namespace classes
+{
+    class BlackJack
+    {
+
+        string input;
+        int playerInput;
+        int userBalance;
+        int playersTotalOfFirstDraw;
+        int playersNextCard;
+        int dealersTotalOfFirstDraw;
+        int playersTotal;
+        int playersHandTotal;
+
+        public void blackJackGame()
+        {
+            Console.WriteLine("Please place your bets £");
+            input = Console.ReadLine();
+            playerInput = Convert.ToInt32(input);
+            Console.WriteLine("You have placed {0}, Dealer has placed {0}", playerInput);
+            playersHand();
+        }
+
+        public void playersHand()
+        {
+            Random rnd = new Random();
+            int playersFirstCard = rnd.Next(1, 12);
+            Console.WriteLine("Your first card is: {0}", playersFirstCard);
+            int playersSecondCard = rnd.Next(1, 12);
+            Console.WriteLine("Your second card is: {0}", playersSecondCard);
+            playersTotal = playersFirstCard + playersSecondCard;
+            Console.WriteLine("Your total is: {0}", playersTotal);
+            PlayerChoices();
+        }    
+        public void PlayerChoices()
+        {   
+            Console.WriteLine("Hit or Stand?");
+            input = Console.ReadLine();
+            Random rnd = new Random();
+            if (input == "Hit")
+            {
+                playersNextCard = rnd.Next(1, 12);
+                Console.WriteLine("Your next card is {0}", playersNextCard);
+                playersTotal += playersNextCard;
+                Console.WriteLine("Your total is: {0}", playersTotal);
+                ScoreCheckPlayer(playersTotal);
+                input = Console.ReadLine();
+                PlayerChoices();
+            }
+            else if (input == "Stand")
+            {
+                playersHandTotal += playersTotal;
+                Console.WriteLine("You have stand at {0}, dealers turn", playersHandTotal);
+                if (playersTotal >= 22)
+            {
+                Console.WriteLine("Bust");
+                dealersWinning();
+            }
+            else if (playersTotal == 21)
+            {
+                Console.WriteLine("BlackJack!!");
+                PlayerWinnings();
+            }
+                Console.Read();
+                dealersHand();
+            }
+        }
+        public void ScoreCheckPlayer(int playersTotal)
+        {
+            if (playersTotal >= 22)
+            {
+                Console.WriteLine("Bust");
+                dealersWinning();
+            }
+            else if (playersTotal == 21)
+            {
+                Console.WriteLine("BlackJack!!");
+                PlayerWinnings();
+            }
+            else
+            {
+                PlayerChoices();
+            }
+        }
+        public void dealersHand()
+        {
+            Random rnd = new Random();
+            int dealersFirstCard = rnd.Next(1, 12);
+            Console.WriteLine("Dealer's first card is: {0}", dealersFirstCard);
+            int dealersSecondCard = rnd.Next(1, 12);
+            Console.WriteLine("Dealer's second card is: {0}", dealersSecondCard);
+            dealersTotalOfFirstDraw = dealersFirstCard += dealersSecondCard;
+            Console.WriteLine("Dealer's total is: {0}", dealersTotalOfFirstDraw);
+            if (dealersTotalOfFirstDraw == 21)
+            {
+                Console.WriteLine("BlackJack!! Player Wins");
+                PlayerWinnings();
+            }
+            else
+            {
+                dealersChoice(dealersTotalOfFirstDraw);
+            }
+        }
+        public void dealersChoice( int dealersTotalOfFirstDraw)
+        {
+            Random rnd = new Random();
+            if (dealersTotalOfFirstDraw < playersHandTotal)
+            {
+                int dealersNextCard = rnd.Next(1, 12);
+                Console.WriteLine("Dealer's next card is {0}", dealersNextCard);
+                dealersTotalOfFirstDraw += dealersNextCard;
+                Console.WriteLine("Dealer's current total is: {0}", dealersTotalOfFirstDraw);
+                ScoreCheckDealer(dealersTotalOfFirstDraw);
+            }
+            else if (dealersTotalOfFirstDraw > playersHandTotal)
+            {
+                Console.WriteLine("Dealer Wins!!");
+                dealersWinning();
+            }
+            else if (dealersTotalOfFirstDraw == playersHandTotal)
+            {
+                Console.WriteLine("Tie");
+            }
+        }
+        public void ScoreCheckDealer(int dealersTotalOfFirstDraw)
+        {
+            if (dealersTotalOfFirstDraw >= 22)
+            {
+                Console.WriteLine("Dealer bust, player wins");
+                PlayerWinnings();
+
+            }
+            else if (dealersTotalOfFirstDraw == 21)
+            {
+                Console.WriteLine("BlackJack!! Dealer wins!!");
+                dealersWinning();
+            }
+            else
+            {
+                dealersChoice(dealersTotalOfFirstDraw);
+            }
+        }
+        public void PlayerWinnings()
+        {
+            playerInput += userBalance;
+            Console.WriteLine("Player wins {0}", playerInput);
+            Console.WriteLine("PlayerWinnings updated balance {0}", userBalance);
+        }
+
+        public void dealersWinning()
+        {
+            playerInput -= userBalance;
+            Console.WriteLine("Dealer wins {0}", playerInput);
+            Console.WriteLine("User balance is: {0}", userBalance);
+        }
+    }
+}
